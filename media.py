370a2ad81a83826e0282d7744f75db7571800853
gudivada amrita siva kumar <38848739+amruthsiva@users.noreply.github.com>
2018-05-22 09:36:45 +0530

    changes

--- media.py
+++ media.py
@@ -5,6 +5,13 @@ print("Content-type:text/html \n")
 
 
 class Movie():
+    '''
+class Movie():
+Attributes:
+        movie_title (str): displays the title of the movie.
+        poster_image (str): one of the look as image.
+        trailer_youtube (str): url of the youtube trailer of the movie.
+'''
     def __init__(self, movie_title, poster_image, trailer_youtube):
         self.title = movie_title
         self.poster_image_url = poster_image
